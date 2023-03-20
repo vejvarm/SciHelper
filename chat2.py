090a44507a74f1d3ad683f32d90896ea47785ca1
vejvarm <vejvarm@gmail.com>
2023-03-20 14:47:34 +0900

    feat: add prompt for article and filter ArXiv article ids to download and process pdfs before conversation starts

--- chat2.py
+++ chat2.py
@@ -14,6 +14,7 @@ from arxiv import arxiv
 from time import time,sleep
 from uuid import uuid4
 from utils import parse_pdf
+from flags import NS, N_TOP_ARTICLES, N_TOP_CONVOS
 
 logging.basicConfig(level=logging.INFO)
 LOGGER = logging.getLogger(__name__)
@@ -49,7 +50,7 @@ def gpt3_embedding(content, engine='text-embedding-ada-002'):
     return vector
 
 
-def gpt3_completion(prompt, engine='text-davinci-003', temp=0.0, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['USER:', 'SciHelper:']):
+def gpt3_completion(prompt, engine='text-davinci-003', temp=0.0, top_p=1.0, tokens=1000, freq_pen=0.0, pres_pen=0.0, stop=['USER:', 'SciHelper:']):
     max_retry = 5
     retry = 0
     prompt = prompt.encode(encoding='ASCII', errors='ignore').decode()
@@ -90,6 +91,13 @@ def load_conversation(results):
     return '\n'.join(messages).strip()
 
 
+def load_articles(results):
+    top_k_list = list()
+    for m in results['matches']:
+        aid = m["id"]
+        top_k_list.append(aid)
+    return top_k_list
+
 def fetch_arxiv_paper(paper_id: str):
     response = arxiv.Search(id_list=[paper_id],
                             max_results=1,
@@ -103,11 +111,14 @@ def fetch_arxiv_paper(paper_id: str):
 
 
 def initialize_conversation(vdb, init_json_path="nexus/user-init.json"):
+    assert init_json_path.endswith(".json")
     json_path = Path(init_json_path)
-    id = init_json_path.split()
+    id = json_path.name.removesuffix(".json")
     if json_path.exists():
         print("Welcome back! My name is SciHelper. I am here to help you with your research. Let's continue our adventure!\n")
-        message = json.load(json_path.open("r"))["message"]
+        metadata = json.load(json_path.open("r"))
+        message = metadata["message"]
+        vector = vdb.fetch([id], namespace=NS.USER_INIT.value)
         # TODO: add user-bio reset option
     else:
         print('\nWelcome, my name is SciHelper. I am here to help you with your research. And you are?\n')
@@ -123,47 +134,59 @@ def initialize_conversation(vdb, init_json_path="nexus/user-init.json"):
         vector = gpt3_embedding(message)
         metadata = {'speaker': 'INIT', 'time': timestamp, 'message': message, 'timestring': timestring, 'uuid': id}
         save_json(json_path, metadata)
-        vdb.upsert([(id, vector)])  # upload to Pinecone
+        vdb.upsert([(id, vector)], namespace=NS.USER_INIT.value)  # upload to Pinecone
 
     # send init to GPT API
     output = gpt3_completion(message)
     LOGGER.info(output)
 
+    return output, vector, metadata
+
 
 def initialize_article(vdb, article_id):
     article_folder = Path("articles/")
+    vector = None
     if article_folder.joinpath(f"{article_id}/metadata.json").exists():
-        metadata = json.load(article_folder.joinpath(f"{article_id}.json").open("r"))
+        metadata = json.load(article_folder.joinpath(f"{article_id}/metadata.json").open("r"))
+        vector = vdb.fetch([article_id], namespace=NS.ARTICLES.value)
         # TODO: load vectors connected to the article from vdb
     else:
         paper = fetch_arxiv_paper(article_id)
-        short_id = paper.get_short_id()
-        metadata = {"id": short_id,
+        metadata = {"id": article_id,
                     "title": paper.title,
-                    "authors": paper.authors,
+                    "authors": [a.name for a in paper.authors],
                     "categories": paper.categories,
-                    "published": paper.published,
+                    "published": paper.published.strftime("%Y-%M-%d"),
                     "journal_ref": paper.journal_ref,
-                    "links": paper.links,
+                    "links": [l.href for l in paper.links],
                     "url": paper.pdf_url,
                     "summary": paper.summary}
-        specific_folder = article_folder.joinpath(short_id)
+        specific_folder = article_folder.joinpath(article_id)
+        specific_folder.mkdir(exist_ok=True)
         json.dump(metadata, specific_folder.joinpath("metadata.json").open("w"), indent=4)
-        LOGGER.info(f"Downloading pdf from ArXiv for article with id: {short_id}")
-        paper.download_pdf(dirpath=specific_folder, filename="paper.pdf")
-        LOGGER.info("Download complete")
+        LOGGER.info(f"Downloading pdf from ArXiv for article with id: {article_id}.\nThis may take a while ...")
+        paper.download_pdf(dirpath=str(specific_folder), filename="paper.pdf")
+        LOGGER.info("... download complete")
 
     # TODO: parse from arxiv source files instead? Maybe into HTML with arxiv-vanity
     text, _ = parse_pdf(article_folder.joinpath(metadata["id"]).joinpath("paper.pdf"))
-    prompt = open_file('prompt_article.txt').replace('<<ARTICLE>>', text)
+
+    # save article as vector into Pinecone
+    # TODO: this might be a lot of information for one vector, we should split it
+    if vector is None:
+        vector = gpt3_embedding(text)
+        vdb.upsert([(article_id, vector)], namespace=NS.ARTICLES.value)
 
     # TODO: continue here,
     #   we will need to send "prompt" through GPT and save the output
-    #   we should vectorize the prompt and save it in pinecone for information retrieval
+    prompt = open_file('prompt_article.txt').replace('<<ARTICLE>>', text)
+    output = gpt3_completion(prompt)
+
+    return output, vector, metadata
 
 
-
-def conversation_turn(speaker: str, message: str, payload: list[tuple[str, float]]):
+def conversation_turn(speaker: str, message: str, payload: list[tuple[str, float]],
+                      n_top_convos=N_TOP_CONVOS, n_top_articles=N_TOP_ARTICLES):
     timestamp = time()
     timestring = timestamp_to_datetime(timestamp)
 
@@ -177,8 +200,10 @@ def conversation_turn(speaker: str, message: str, payload: list[tuple[str, float
         output = None
     else:
         # search for relevant messages, and generate a response
-        results = vdb.query(vector=vector, top_k=convo_length)
-        conversation = load_conversation(results)  # results should be a DICT with 'matches' which is a LIST of DICTS, with 'id'
+        prev_convos = vdb.query(vector=vector, top_k=n_top_convos, namespace=NS.CONVOS.value)
+        prev_articles = vdb.query(vector=vector, top_k=n_top_articles, namespace=NS.ARTICLES.value)
+        conversation = load_conversation(prev_convos)  # results should be a DICT with 'matches' which is a LIST of DICTS, with 'id'
+        article_ids = load_articles(prev_articles)  # TODO: now what do we actually do with these?, we should cache the article texts I guess
         prompt = open_file('prompt_response.txt').replace('<<CONVERSATION>>', conversation).replace('<<MESSAGE>>', message)
         # generate response, vectorize, save, etc
         output = gpt3_completion(prompt)
@@ -186,8 +211,31 @@ def conversation_turn(speaker: str, message: str, payload: list[tuple[str, float
     return output, vector, metadata
 
 
+def prompt_for_article():
+    while True:
+        usr_input = input("Please give me a valid ArXiv article address or ID: ")
+
+        article_id = _extract_arxiv_id(usr_input)
+
+        if article_id:
+            print("Thank you!")
+            return article_id
+        else:
+            print("I couldn't find any article id in your input. Can you recheck and try again?\n")
+            # TODO: "Would you like to continue without a specific article?"
+
+
+def _extract_arxiv_id(string):
+    pattern = r"(?:(?:arXiv:)?([0-9]{2}[0-1][0-9]\.[0-9]{4,5}(?:v[0-9]+)?|(?:[a-z|\-]+(\.[A-Z]{2})?\/\d{2}[0-1][0-9]\d{3})))"
+    # TODO: maybe ignore versions
+    match = re.search(pattern, string.strip())
+    if match:
+        return match.group(1)
+    else:
+        return None
+
+
 if __name__ == '__main__':
-    convo_length = 5
     openai.api_key = open_file('key_openai.txt')
     pinecone.init(api_key=open_file('key_pinecone.txt'), environment='us-east-1-aws')
     vdb = pinecone.Index("llm-ltm")
@@ -196,8 +244,8 @@ if __name__ == '__main__':
     initialize_conversation(vdb)
 
     # initialize article
-    input("Please provide valid ArXiv article ID: ") # TODO: support full urls as well
-    initialize_article(vdb)
+    article_id = prompt_for_article()
+    initialize_article(vdb, article_id)
     # TODO: this should be repeatable (maybe different thread and invoke at specific USER prompt?)
     #   when we have web-ui, we can use a button for this
 
@@ -210,5 +258,5 @@ if __name__ == '__main__':
 
         # SciHelper turn
         _, _, _ = conversation_turn("SciHelper", output, payload)
-        vdb.upsert(payload)
+        vdb.upsert(payload, namespace=NS.CONVOS.value)
         print('\n\nSciHelper: %s' % output)
---
+++ flags.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class NS(Enum):
+    USER_INIT = 'user-init'
+    ARTICLES = 'articles'
+    CONVOS = 'convos'
+
+N_TOP_CONVOS = 5
+N_TOP_ARTICLES = 3
